1ecfd56ca62bc01349360a5133c3df2110d860f6
Alan Shaw <alan.shaw@protocol.ai>
2019-05-17 22:29:17 +0100

    feat: lite
    License: MIT
    Signed-off-by: Alan Shaw <alan.shaw@protocol.ai>

--- src/index.js
+++ src/index.js
@@ -5,68 +5,35 @@
 'use strict'
 
 const mh = require('multihashes')
-const cryptoKeys = require('libp2p-crypto/src/keys')
 const assert = require('assert')
-const waterfall = require('async/waterfall')
 const withIs = require('class-is')
 
-class PeerId {
+class _PeerId {
   constructor (id, privKey, pubKey) {
     assert(Buffer.isBuffer(id), 'invalid id provided')
-
-    if (privKey && pubKey) {
-      assert(privKey.public.bytes.equals(pubKey.bytes), 'inconsistent arguments')
-    }
-
+    assert(arguments.length === 1, 'private/public key arguments unsupported in lite')
     this._id = id
     this._idB58String = mh.toB58String(this.id)
-    this._privKey = privKey
-    this._pubKey = pubKey
   }
 
   get id () {
     return this._id
   }
 
-  set id (val) {
+  set id (_) {
     throw new Error('Id is immutable')
   }
 
-  get privKey () {
-    return this._privKey
+  set privKey (_) {
+    throw new Error('set private key unsupported in lite')
   }
 
-  set privKey (privKey) {
-    this._privKey = privKey
+  set pubKey (_) {
+    throw new Error('set public key unsupported in lite')
   }
 
-  get pubKey () {
-    if (this._pubKey) {
-      return this._pubKey
-    }
-
-    if (this._privKey) {
-      return this._privKey.public
-    }
-  }
-
-  set pubKey (pubKey) {
-    this._pubKey = pubKey
-  }
-
-  // Return the protobuf version of the public key, matching go ipfs formatting
-  marshalPubKey () {
-    if (this.pubKey) {
-      return cryptoKeys.marshalPublicKey(this.pubKey)
-    }
-  }
-
-  // Return the protobuf version of the private key, matching go ipfs formatting
-  marshalPrivKey () {
-    if (this.privKey) {
-      return cryptoKeys.marshalPrivateKey(this.privKey)
-    }
-  }
+  marshalPubKey () {}
+  marshalPrivKey () {}
 
   toPrint () {
     let pid = this.toB58String()
@@ -86,11 +53,7 @@ class PeerId {
   // return the jsonified version of the key, matching the formatting
   // of go-ipfs for its config file
   toJSON () {
-    return {
-      id: this.toB58String(),
-      privKey: toB64Opt(this.marshalPrivKey()),
-      pubKey: toB64Opt(this.marshalPubKey())
-    }
+    return { id: this.toB58String() }
   }
 
   // encode/decode functions
@@ -116,186 +79,71 @@ class PeerId {
     }
   }
 
-  /*
-   * Check if this PeerId instance is valid (privKey -> pubKey -> Id)
-   */
   isValid (callback) {
-    // TODO Needs better checking
-    if (this.privKey &&
-      this.privKey.public &&
-      this.privKey.public.bytes &&
-      Buffer.isBuffer(this.pubKey.bytes) &&
-      this.privKey.public.bytes.equals(this.pubKey.bytes)) {
-      callback()
-    } else {
-      callback(new Error('Keys not match'))
+    callback(new Error('Keys not match'))
+  }
+
+  static create (options, callback) {
+    if (typeof options === 'function') {
+      callback = options
+      options = {}
     }
+
+    callback(new Error('not supported in lite'))
   }
-}
 
-const PeerIdWithIs = withIs(PeerId, { className: 'PeerId', symbolName: '@libp2p/js-peer-id/PeerId' })
-
-exports = module.exports = PeerIdWithIs
-
-// generation
-exports.create = function (opts, callback) {
-  if (typeof opts === 'function') {
-    callback = opts
-    opts = {}
+  static createFromHexString (str) {
+    return new PeerId(mh.fromHexString(str))
   }
-  opts = opts || {}
-  opts.bits = opts.bits || 2048
 
-  waterfall([
-    (cb) => cryptoKeys.generateKeyPair('RSA', opts.bits, cb),
-    (privKey, cb) => privKey.public.hash((err, digest) => {
-      cb(err, digest, privKey)
-    })
-  ], (err, digest, privKey) => {
-    if (err) {
+  static createFromBytes (buf) {
+    return new PeerId(buf)
+  }
+
+  static createFromB58String (str) {
+    return new PeerId(mh.fromB58String(str))
+  }
+
+  static createFromPubKey (_, callback) {
+    callback(new Error('not supported in lite'))
+  }
+
+  static createFromPrivKey (_, callback) {
+    callback(new Error('not supported in lite'))
+  }
+
+  static createFromJSON (obj, callback) {
+    if (typeof callback !== 'function') {
+      throw new Error('callback is required')
+    }
+
+    let id
+
+    try {
+      id = mh.fromB58String(obj.id)
+
+      if (obj.privKey) {
+        throw new Error('create from JSON with private key not supported in lite')
+      }
+
+      if (obj.pubKey) {
+        throw new Error('create from JSON with public key not supported in lite')
+      }
+    } catch (err) {
       return callback(err)
     }
 
-    callback(null, new PeerIdWithIs(digest, privKey))
-  })
-}
-
-exports.createFromHexString = function (str) {
-  return new PeerIdWithIs(mh.fromHexString(str))
-}
-
-exports.createFromBytes = function (buf) {
-  return new PeerIdWithIs(buf)
-}
-
-exports.createFromB58String = function (str) {
-  return new PeerIdWithIs(mh.fromB58String(str))
-}
-
-// Public Key input will be a buffer
-exports.createFromPubKey = function (key, callback) {
-  if (typeof callback !== 'function') {
-    throw new Error('callback is required')
-  }
-
-  let pubKey
-
-  try {
-    let buf = key
-    if (typeof buf === 'string') {
-      buf = Buffer.from(key, 'base64')
-    }
-
-    if (!Buffer.isBuffer(buf)) throw new Error('Supplied key is neither a base64 string nor a buffer')
-
-    pubKey = cryptoKeys.unmarshalPublicKey(buf)
-  } catch (err) {
-    return callback(err)
-  }
-
-  pubKey.hash((err, digest) => {
-    if (err) {
-      return callback(err)
-    }
-
-    callback(null, new PeerIdWithIs(digest, null, pubKey))
-  })
-}
-
-// Private key input will be a string
-exports.createFromPrivKey = function (key, callback) {
-  if (typeof callback !== 'function') {
-    throw new Error('callback is required')
-  }
-
-  let buf = key
-
-  try {
-    if (typeof buf === 'string') {
-      buf = Buffer.from(key, 'base64')
-    }
-
-    if (!Buffer.isBuffer(buf)) throw new Error('Supplied key is neither a base64 string nor a buffer')
-  } catch (err) {
-    return callback(err)
-  }
-
-  waterfall([
-    (cb) => cryptoKeys.unmarshalPrivateKey(buf, cb),
-    (privKey, cb) => privKey.public.hash((err, digest) => {
-      cb(err, digest, privKey)
-    })
-  ], (err, digest, privKey) => {
-    if (err) {
-      return callback(err)
-    }
-
-    callback(null, new PeerIdWithIs(digest, privKey, privKey.public))
-  })
-}
-
-exports.createFromJSON = function (obj, callback) {
-  if (typeof callback !== 'function') {
-    throw new Error('callback is required')
-  }
-
-  let id
-  let rawPrivKey
-  let rawPubKey
-  let pub
-
-  try {
-    id = mh.fromB58String(obj.id)
-    rawPrivKey = obj.privKey && Buffer.from(obj.privKey, 'base64')
-    rawPubKey = obj.pubKey && Buffer.from(obj.pubKey, 'base64')
-    pub = rawPubKey && cryptoKeys.unmarshalPublicKey(rawPubKey)
-  } catch (err) {
-    return callback(err)
-  }
-
-  if (rawPrivKey) {
-    waterfall([
-      (cb) => cryptoKeys.unmarshalPrivateKey(rawPrivKey, cb),
-      (priv, cb) => priv.public.hash((err, digest) => {
-        cb(err, digest, priv)
-      }),
-      (privDigest, priv, cb) => {
-        if (pub) {
-          pub.hash((err, pubDigest) => {
-            cb(err, privDigest, priv, pubDigest)
-          })
-        } else {
-          cb(null, privDigest, priv)
-        }
-      }
-    ], (err, privDigest, priv, pubDigest) => {
-      if (err) {
-        return callback(err)
-      }
-
-      if (pub && !privDigest.equals(pubDigest)) {
-        return callback(new Error('Public and private key do not match'))
-      }
-
-      if (id && !privDigest.equals(id)) {
-        return callback(new Error('Id and private key do not match'))
-      }
-
-      callback(null, new PeerIdWithIs(id, priv, pub))
-    })
-  } else {
-    callback(null, new PeerIdWithIs(id, null, pub))
+    callback(null, new PeerId(id))
   }
 }
 
-exports.isPeerId = function (peerId) {
+const PeerId = withIs(_PeerId, { className: 'PeerId', symbolName: '@libp2p/js-peer-id/PeerId' })
+
+// FIXME: this overwrites withIs
+PeerId.isPeerId = function (peerId) {
   return Boolean(typeof peerId === 'object' &&
     peerId._id &&
     peerId._idB58String)
 }
 
-function toB64Opt (val) {
-  if (val) {
-    return val.toString('base64')
-  }
-}
+module.exports = PeerId
